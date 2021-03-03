6cbc49f63824e45dfe17655c6a71193b0f4cc232
mitch092 <stevenm7861@gmail.com>
2021-03-02 17:54:56 -0800

    Added unity build.

--- build.ps1
+++ build.ps1
@@ -5,7 +5,8 @@
 
 param (
     [switch]$release = $false,
-    [switch]$config = $false
+    [switch]$config = $false,
+    [switch]$unity = $false
 )
 
 [string]$build_folder = "debug"
@@ -16,11 +17,16 @@ if($release){
 }
 [string]$builds = "${PSScriptRoot}\builds\${build_folder}_msvc_ninja"
 
+[string]$unity_str = ""
+if($unity){
+    $unity_str = "-DCMAKE_UNITY_BUILD=ON -CMAKE_UNITY_BUILD_BATCH_SIZE=0"
+}
+
 if($config){    
     Remove-Item $builds -Force -Recurse -ErrorAction SilentlyContinue
     New-Item -Path $builds -ItemType Directory
     Push-Location -Path $builds    
-    iex "cmake -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl -DCMAKE_BUILD_TYPE=${cmake_build} -G `"Ninja`" ../../"
+    iex "cmake -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl -DCMAKE_BUILD_TYPE=${cmake_build} ${unity_str} -G `"Ninja`" ../../"
     Pop-Location
 }
 
